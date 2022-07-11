509e360e380eaddb92db1ecda6b11987775ebfc0
Vishal Yadav <vishalyadav1746@gmail.com>
2022-07-12 00:02:49 +0530

    chapter 1: update solution - String rotation.

--- src/org/vishal1297/ds/chapter_1/problems/StringRotation.java
+++ src/org/vishal1297/ds/chapter_1/problems/StringRotation.java
@@ -3,20 +3,12 @@ package org.vishal1297.ds.chapter_1.problems;
 public class StringRotation {
 
     public static void main(String[] args) {
-        System.out.println(isRotation("waterbottle", "erbottlewat"));
+        System.out.println(isRotation("bottlewater", "erbottlewat"));
     }
 
     public static boolean isRotation(String s1, String s2) {
-        if (s1.length() != s2.length()) return false;
-        int splitIndex = -1;
-        for (int i = 0; i < s1.length(); i++) {
-            if (s1.charAt(0) == s2.charAt(i)) {
-                splitIndex = i;
-            }
-        }
-        if (splitIndex == -1) return false;
-        String splitOne = s2.substring(0, splitIndex);
-        return isSubstring(s2 + splitOne, s1);
+        if (s1.length() == 0 || s1.length() != s2.length()) return false;
+        return isSubstring(s1 + s1, s2);
     }
 
     public static boolean isSubstring(String s1, String s2) {
